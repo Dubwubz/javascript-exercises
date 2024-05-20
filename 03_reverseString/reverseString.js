be86e2905768d89f10efd5faacb3778b8b5be836
Nathan Astley <22crazyfrog@gmail.com>
2024-05-19 17:30:30 -0800

    3rd Assignment Finished!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let string = "";
+    for (let i = words.length - 1; i >= 0; i--) {
+        string += words[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
